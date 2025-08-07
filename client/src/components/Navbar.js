a37d9e2560aed22e5a4f767550113e2d89e2fd11
Mickey Gray <mickeygray85@hotmail.com>
2025-08-07 14:07:37 -0700

    thankyou

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,9 +10,12 @@ const Navbar = () => {
 
   // Check if the current path is /qualify-now
   const isQualifyNow = location.pathname === "/qualify-now";
+  const isThankYou = location.pathname === "/thank-you";
 
-  const phoneNumber = isQualifyNow ? "+18667163921" : "+18449966829";
-  const phoneDisplay = isQualifyNow ? "866-716-3921" : "(844) 996-6829";
+  const phoneNumber =
+    isQualifyNow || isThankYou ? "+18667163921" : "+18449966829";
+  const phoneDisplay =
+    isQualifyNow || isThankYou ? "866-716-3921" : "(844) 996-6829";
   // Detect screen width changes for responsive behavior
   useEffect(() => {
     const handleResize = () => {
