83bfd2a3dc80c78618cb7bbdebdb984b5a545fca
Mickey Gray <mickeygray85@hotmail.com>
2026-02-26 12:50:29 -0800

    big patch

--- client/scripts/generate-sitemap.js
+++ client/scripts/generate-sitemap.js
@@ -7,6 +7,60 @@ const blogData = require("../src/data/blogData");
 
 const DOMAIN = "https://www.wynntaxsolutions.com";
 
+const states = [
+  "alabama",
+  "alaska",
+  "arizona",
+  "arkansas",
+  "california",
+  "colorado",
+  "connecticut",
+  "delaware",
+  "florida",
+  "georgia",
+  "hawaii",
+  "idaho",
+  "illinois",
+  "indiana",
+  "iowa",
+  "kansas",
+  "kentucky",
+  "louisiana",
+  "maine",
+  "maryland",
+  "massachusetts",
+  "michigan",
+  "minnesota",
+  "mississippi",
+  "missouri",
+  "montana",
+  "nebraska",
+  "nevada",
+  "new-hampshire",
+  "new-jersey",
+  "new-mexico",
+  "new-york",
+  "north-carolina",
+  "north-dakota",
+  "ohio",
+  "oklahoma",
+  "oregon",
+  "pennsylvania",
+  "rhode-island",
+  "south-carolina",
+  "south-dakota",
+  "tennessee",
+  "texas",
+  "utah",
+  "vermont",
+  "virginia",
+  "washington",
+  "west-virginia",
+  "wisconsin",
+  "wyoming",
+  "district-of-columbia",
+];
+
 const staticPages = [
   { loc: "/", priority: "1.0", changefreq: "daily" },
   { loc: "/our-tax-services", priority: "0.8", changefreq: "monthly" },
@@ -17,6 +71,8 @@ const staticPages = [
   { loc: "/tax-relief", priority: "0.8", changefreq: "monthly" },
   { loc: "/tax-resolution", priority: "0.8", changefreq: "monthly" },
   { loc: "/tax-negotiation", priority: "0.8", changefreq: "monthly" },
+  { loc: "/tax-protection-plans", priority: "0.7", changefreq: "monthly" },
+  { loc: "/state-tax-guide", priority: "0.8", changefreq: "monthly" },
   { loc: "/privacy-policy", priority: "0.2", changefreq: "yearly" },
   { loc: "/terms-of-service", priority: "0.2", changefreq: "yearly" },
 ];
@@ -40,10 +96,19 @@ const blogEntries = blogData.map((blog) =>
   }),
 );
 
+const stateEntries = states.map((s) =>
+  toEntry({
+    loc: `/state-tax-guide/state-income-tax-help-${s}`,
+    priority: "0.6",
+    changefreq: "monthly",
+  }),
+);
+
 const allEntries = [
   ...staticPages.map(toEntry),
   ...subPageEntries,
   ...blogEntries,
+  ...stateEntries,
 ];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
